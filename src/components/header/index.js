dbbe66a957791fe653df22e63a5929641fcc091d
Felipe paiva <Felipe.paiva@fh.com.br>
2020-07-02 09:51:47 -0300

    commit 2

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+
+const Header = () => {
+    let header = <header id="main-header"> JS Hunt </header>
+    return header
+};
+
+export default Header;
